86b44a4c449d307686353a815ccc8ada5a937710
ZZMMDDgit <1550282756@qq.com>
2025-10-02 22:27:35 +0800

    lask

--- ConsoleApplication1/FileName.c
+++ ConsoleApplication1/FileName.c
@@ -607,3 +607,49 @@ int main() {
         printf("%d", err[i]);
     }
 }*/
+
+/*#include<stdio.h>
+
+int main(){
+    int a, b = 0, c[5];
+    scanf("%d", &a);
+    while (a > 0) {
+        c[b] = a % 10;
+        a /= 10;
+        b++;
+    }
+    printf("%d\n", b);
+    for (int i = b; i > 0; i--) {
+
+        printf("%d ", c[i - 1]);
+
+    }
+    printf("\n");
+    for (int i = 0; i < b; i++) {
+
+        printf("%d", c[i]);
+
+    }
+}*/
+
+
+#include<stdio.h>
+
+int main() {
+    int a, b=0, c[5];
+    scanf("%d",&a);
+    while (a > 0) {
+        c[b] = a % 10;
+        a = (a - c[b]) / 10;
+        b++;
+    }
+    printf("%d\n", b);
+    for (int i = (b-1); i >= 0; i--) {
+        printf("%d ", c[i]);
+    }
+    printf("\n");
+    
+    for (int i = 0; i < 5; i++) {
+        printf("%d", c[i]);
+    }
+}
